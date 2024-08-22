88d8b148745bb862e30697438ecca5c8cb28ac35
milosdjurica <milosdjurica.work@gmail.com>
2024-08-22 20:22:16 +0200

    config

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -9,18 +9,30 @@ pub use pallet::*;
 pub mod pallet {
  use frame_support::pallet_prelude::*;
  use frame_system::pallet_prelude::*;
+	use scale_info::prelude::vec::Vec;
+
+
+
+
+
 
  #[pallet::pallet]
+ 
+	#[pallet::without_storage_info]
  pub struct Pallet<T>(_);
 
- #[pallet::config]  // <-- Step 2. code block will replace this.
+#[pallet::config]
+	pub trait Config: frame_system::Config + TimestampConfig {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		type Time: From<<Self as TimestampConfig>::Moment>;
+	}
+
+
+
+
  #[pallet::event]   // <-- Step 3. code block will replace this.
  #[pallet::error]   // <-- Step 4. code block will replace this.
  #[pallet::storage] // <-- Step 5. code block will replace this.
  #[pallet::call]    // <-- Step 6. code block will replace this.
 }
 
-pub mod weights {
-  // Placeholder struct for the pallet weights
-  pub struct SubstrateWeight<T>(core::marker::PhantomData<T>);
-}
